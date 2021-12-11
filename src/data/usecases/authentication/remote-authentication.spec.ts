19b622a67d960d727e8b7f2fb631704ae489c062
Gerson Dantas <gerson.accari@hotmail.com>
2021-12-11 15:19:48 -0300

    fix: typo

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -71,6 +71,6 @@ describe('RemoteAuthentication', () => {
       body: httpResult
     }
     const account = await sut.auth(mockAuthentication())
-    await expect(account).toEqual(httpResult)
+    expect(account).toEqual(httpResult)
   })
 })
