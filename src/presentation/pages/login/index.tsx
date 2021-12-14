0b3a10b7e4634fe683d7f6d36b30d04b5d2a889c
Gerson Dantas <gerson.accari@hotmail.com>
2021-12-14 13:59:57 -0300

    refactor: refactor in public past

---
+++ src/presentation/pages/login/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Styles from './login-styles.scss'
+
+const login: React.FC = () => {
+  return <div className={Styles.login}>hello!</div>
+}
+
+export default login
