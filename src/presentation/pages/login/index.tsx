e6ff05678cfeca955701d46ec63a785de71f21be
Gerson Dantasy <gerson.accari@hotmail.com>
2021-12-27 11:00:46 -0300

    refactor: move input to separate file

--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -4,6 +4,7 @@ import Spinner from '@/presentation/components/spinner'
 import Logo from '@/presentation/components/logo'
 import Header from '@/presentation/components/login-header'
 import Footer from '@/presentation/components/footer'
+import Input from '@/presentation/components/input'
 
 const login: React.FC = () => {
   return (
@@ -11,25 +12,15 @@ const login: React.FC = () => {
       <Header />
       <form className={Styles.form}>
         <h2>Login</h2>
-        <div className={Styles.inputWrap}>
-          <input type="email" name="email" placeholder="Digite seu email" />
-          <span className={Styles.status}>🔴</span>
-        </div>
-        <div className={Styles.inputWrap}>
-          <input
-            type="password"
-            name="password"
-            placeholder="Digite sua senha"
-          />
-          <span className={Styles.status}>🔴</span>
-        </div>
+        <Input type="email" name="email" placeholder="Digite seu email" />
+        <Input type="password" name="password" placeholder="Digite sua senha" />
         <button className={Styles.submit} type="submit">
           Entrar
         </button>
 
         <span className={Styles.link}>Criar Conta</span>
         <div className={Styles.errorWrap}>
-          <Spinner className={Styles.spinner}/>
+          <Spinner className={Styles.spinner} />
           <span className={Styles.error}>Erro</span>
         </div>
       </form>
