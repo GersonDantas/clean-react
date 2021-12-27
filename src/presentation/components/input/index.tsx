3bf4d767bb66536c489fbdf2900c872a59259b13
Gerson Dantasy <gerson.accari@hotmail.com>
2021-12-27 14:35:14 -0300

    feat: remove autoFill

--- src/presentation/components/input/index.tsx
+++ src/presentation/components/input/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Styles from './input-styles.scss'
 
-type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+type Props = React.DetailedHTMLProps<
+React.InputHTMLAttributes<HTMLInputElement>,
+HTMLInputElement
+>
 
 const Input: React.FC<Props> = (props: Props) => {
+  const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
+    event.target.readOnly = false
+  }
+
   return (
     <div className={Styles.inputWrap}>
-      <input {...props} />
+      <input {...props} onFocus={enableInput} readOnly />
       <span className={Styles.status}>🔴</span>
     </div>
   )
