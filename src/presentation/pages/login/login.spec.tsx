57983f4e747f9406e099ca416d99c01c25444c14
Gerson Dantasy <gerson.accari@hotmail.com>
2021-12-28 19:28:08 -0300

    test: ensure Login present spinner on submit

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -89,4 +89,16 @@ describe('Login component', () => {
     const submitButton = sut.getByTestId('submit') as HTMLButtonElement // fazendo cache para button
     expect(submitButton.disabled).toBe(false)
   })
+
+  test('Should show spinner on submit ', () => {
+    const { sut } = makeSut()
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: faker.internet.password() } })
+    const submitButton = sut.getByTestId('submit')
+    fireEvent.click(submitButton)
+    const spinner = sut.getByTestId('spinner')
+    expect(spinner).toBeTruthy()
+  })
 })
