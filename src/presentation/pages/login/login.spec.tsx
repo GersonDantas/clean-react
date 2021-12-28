457f08f62a13b6d6bef54a14bfecb6346268131b
Gerson Dantasy <gerson.accari@hotmail.com>
2021-12-28 14:02:58 -0300

    refactor: move SUT  to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import { Login } from '..'
+
+type SutTypes = {
+  sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />)
+  return { sut }
+}
 
 describe('Login component', () => {
   test('Should start initial state', () => {
-    const { getByTestId } = render(<Login />)
-    const errorWrap = getByTestId('error-wrap')
+    const { sut } = makeSut()
+    const errorWrap = sut.getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
-    const submitButton = getByTestId('submit') as HTMLButtonElement // fazendo cache para button
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement // fazendo cache para button
     expect(submitButton.disabled).toBe(true)
+    const emailStatus = sut.getByTestId('email-status')
+    expect(emailStatus.title).toBe('Campo obrigatório')
+    expect(emailStatus.textContent).toBe('🔴')
+    const passwordStatus = sut.getByTestId('password-status')
+    expect(passwordStatus.title).toBe('Campo obrigatório')
+    expect(passwordStatus.textContent).toBe('🔴')
   })
 })
