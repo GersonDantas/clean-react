488709852984cfb761e88caee4550b94bb7ad327
Gerson Dantasy <gerson.accari@hotmail.com>
2021-12-28 17:07:02 -0300

    feat: ensure Login present valid state if password is invalid

--- src/presentation/components/input/index.tsx
+++ src/presentation/components/input/index.tsx
@@ -22,11 +22,11 @@ const Input: React.FC<Props> = (props: Props) => {
   }
 
   const getStatus = (): string => {
-    return '🔴'
+    return error ? '🔴' : '🟢'
   }
 
   const getError = (): string => {
-    return error
+    return error || 'Tudo certo!'
   }
 
   return (
