71c857bfac549e1dfaba22355d9de2cf962501b9
Gerson Dantasy <gerson.accari@hotmail.com>
2021-12-29 15:09:30 -0300

    feat: prevent Login tosubmit form if its invalid

--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -31,7 +31,7 @@ const login: React.FC<Props> = ({ validation, authentication }: Props) => {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
 
-    if (state.isLoading) {
+    if (state.isLoading || state.emailError || state.passwordError) {
       return
     }
     setState({
@@ -48,7 +48,7 @@ const login: React.FC<Props> = ({ validation, authentication }: Props) => {
     <div className={Styles.login}>
       <Header />
       <Context.Provider value={{ state, setState }}>
-        <form className={Styles.form} onSubmit={handleSubmit}>
+        <form data-testid="form" className={Styles.form} onSubmit={handleSubmit}>
           <h2>Login</h2>
           <Input type="email" name="email" placeholder="Digite seu email" />
           <Input type="password" name="password" placeholder="Digite sua senha" />
