39a0687e38da507b77ed6cc9fe6a2bc0788ac403
Gerson Dantasy <gerson.accari@hotmail.com>
2021-12-30 11:35:32 -0300

    feat: ensure Login navigates signup cprrectly

--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import Styles from './login-styles.scss'
 import { Footer, FormStatus, Header, Input, Logo, Spinner } from '@/presentation/components'
 import Context from '@/presentation/context/form'
@@ -72,7 +73,7 @@ const login: React.FC<Props> = ({ validation, authentication }: Props) => {
             Entrar
           </button>
 
-          <span className={Styles.link}>Criar Conta</span>
+          <Link data-testid="signup" to="/signup" className={Styles.link}>Criar Conta</Link>
 
           <FormStatus />
         </form>
