76dd337c751f74668b339aa9ffe1677768496a6a
Gerson Dantasy <gerson.accari@hotmail.com>
2022-01-03 15:52:30 -0300

    feat: ensure ValidationComposite error if any validation fails

--- src/infra/http/test/mock-axios.ts
+++ src/infra/http/test/mock-axios.ts
@@ -5,7 +5,7 @@ export const mockAxios = (): jest.Mocked<typeof axios> => {
   const mockedAxios = axios as jest.Mocked<typeof axios>
   mockedAxios.post.mockResolvedValue({
     data: faker.random.objectElement(),
-    status: faker.random.number()
+    status: faker.datatype.number()
   })
 
   return mockedAxios
