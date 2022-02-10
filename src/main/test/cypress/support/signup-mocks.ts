a3b0b13ae67cef7cf808f56165ff2bb25970c5af
Gerson Dantas <gerson.accari@hotmail.com>
2022-02-10 16:43:52 -0300

    test: ensure SignUp present correct error if server fails

--- src/main/test/cypress/support/signup-mocks.ts
+++ src/main/test/cypress/support/signup-mocks.ts
@@ -2,3 +2,4 @@ import * as Helper from './http-mocks'
 import faker from 'faker'
 
 export const mockEmailInUseError = (): void => Helper.mockEmailInUseError(/signup/)
+export const mockUnexpectedError = (): void => Helper.mockUnexpectedError(/signup/, 'POST')
