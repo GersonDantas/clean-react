1806d69fee4926a31149247fcf2753bc75437862
Gerson Dantas <gerson.accari@hotmail.com>
2022-02-10 16:49:11 -0300

    test: ensure SignUp save accesstoken on localStorage on success

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -76,4 +76,11 @@ describe('SignUp', () => {
     FormHelper.testMainError('Algo de errado aconteceu. Tente novamente em breve')
     FormHelper.testUrl('/signup')
   })
+
+  it('Should present save accessToken if valid credentials are provided', () => {
+    Http.mockOk()
+    simulateValidSubmit()
+    FormHelper.testUrl('/')
+    FormHelper.testLocalStorageItem('accessToken')
+  })
 })
