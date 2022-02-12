aa8582f3a64cff6e2f27737532ec3dd209b52202
Gerson Dantas <gerson.accari@hotmail.com>
2022-02-12 14:21:08 -0300

    test: ensure RemoteLoadSurveiList calls HttGetClient with correct URL

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -1,4 +1,6 @@
 import {
+  HttpGetClient,
+  HttpGetParams,
   HttpPostClient,
   HttpPostParams,
   HttpResponse,
@@ -22,5 +24,13 @@ export class HttpPostClientSpy<R> implements HttpPostClient<R> {
     this.url = params.url
     this.body = params.body
     return Promise.resolve(this.response)
+  }
+}
+
+export class HttpGetClientSpy implements HttpGetClient {
+  url: string
+
+  async get (params: HttpGetParams): Promise<void> {
+    this.url = params.url
   }
 }
