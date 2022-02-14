c6dcba6436287543281b13156509dcf0d3dbb196
Gerson Dantas <gerson.accari@hotmail.com>
2022-02-14 14:20:31 -0300

    feat: ensure AxiosHttpClient returns correct values on success

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -15,7 +15,11 @@ export class AxiosHttpClient implements HttpPostClient {
     }
   }
 
-  async get (params: HttpGetParams): Promise<void> {
-    await axios.get(params.url)
+  async get (params: HttpGetParams): Promise<HttpResponse> {
+    const axiosResponse = await axios.get(params.url)
+    return {
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data
+    }
   }
 }
