7e6b48b176dbda81a0cc60e6ad5919930aad0d4b
Gerson Dantas <gerson.accari@hotmail.com>
2022-02-21 17:29:08 -0300

    feat: ensure getCurrentAccountAdapter

--- src/main/factories/cache/local-storage-adapter-factory.ts
+++ src/main/factories/cache/local-storage-adapter-factory.ts
@@ -1,6 +1,6 @@
 import { SetStorage } from '@/data/protocols/cache/set-storage'
 import { LocalStorageAdapter } from '@/infra/http/cache/local-storage-adapter'
 
-export const makeLocalStorageAdapter = (): SetStorage => {
+export const makeLocalStorageAdapter = (): LocalStorageAdapter => {
   return new LocalStorageAdapter()
 }
