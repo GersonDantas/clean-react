bfdf855d4965327d8c1378486ce159ad298f0c54
Gerson Dantas <gerson.accari@hotmail.com>
2022-02-24 17:49:12 -0300

    test: ensure AuthorizeHttpGetClientDecorator not add token if GetStorage is invalid

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -18,7 +18,7 @@ export const mockGetRequest = (): HttpGetParams => ({
   headers: faker.random.objectElement()
 })
 
-export class HttpPostClientSpy<R> implements HttpPostClient<R> {
+export class HttpPostClientSpy<R = any> implements HttpPostClient<R> {
   url?: string
   body?: any
   response: HttpResponse<R> = {
@@ -32,14 +32,16 @@ export class HttpPostClientSpy<R> implements HttpPostClient<R> {
   }
 }
 
-export class HttpGetClientSpy<R> implements HttpGetClient<R> {
+export class HttpGetClientSpy<R = any> implements HttpGetClient<R> {
   url: string
+  headers?: any
   response: HttpResponse<R> = {
     statusCode: HttpStatusCode.ok
   }
 
   async get (params: HttpGetParams): Promise<HttpResponse<R>> {
     this.url = params.url
+    this.headers = params.headers
     return this.response
   }
 }
