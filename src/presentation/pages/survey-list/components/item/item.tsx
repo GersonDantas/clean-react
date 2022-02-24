2035be9283a85c12c188a9788b51df223da66e3d
Gerson Dantas <gerson.accari@hotmail.com>
2022-02-24 12:46:41 -0300

    refactor: move SurveyList code to components

--- src/presentation/pages/survey-list/components/item/item.tsx
+++ src/presentation/pages/survey-list/components/item/item.tsx
@@ -1,4 +1,4 @@
-import Styles from './survey-item-styles.scss'
+import Styles from './item-styles.scss'
 import { Icon, IconName } from '@/presentation/components'
 import { SurveyModel } from '@/domain/models'
 import React from 'react'
--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,9 +1,9 @@
 import Styles from './survey-list-styles.scss'
+import { SurveyContext, SurveyListItem, Error } from '@/presentation/pages/survey-list/components'
 import { Footer, Header } from '@/presentation/components'
 import { LoadSurveyList } from '@/domain/usecases'
-import { SurveyItem, SurveyItemEmpty } from '@/presentation/pages/survey-list/components'
-import React, { useEffect, useState } from 'react'
 import { SurveyModel } from '@/domain/models'
+import React, { useEffect, useState } from 'react'
 
 type Props = {
   loadSurveyList: LoadSurveyList
@@ -24,18 +24,9 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
       <Header />
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
-        {state.error
-          ? <div>
-            <span data-testid="error">{state.error}</span>
-            <button>Recarregar</button>
-          </div>
-          : <ul data-testid="survey-list">
-            {state.surveys.length
-              ? state.surveys.map((survey: SurveyModel) => <SurveyItem key={survey.id} survey={survey} />)
-              : <SurveyItemEmpty />
-            }
-          </ul>
-        }
+        <SurveyContext.Provider value={{ state, setState }}>
+          {state.error ? <Error /> : <SurveyListItem />}
+        </SurveyContext.Provider>
       </div>
       <Footer />
     </div>
