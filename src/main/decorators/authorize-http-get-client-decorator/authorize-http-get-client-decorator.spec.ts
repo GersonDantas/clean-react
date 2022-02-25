1cc52b3008fcb8591c40ccac12f607469ad51a5b
Gerson Dantas <gerson.accari@hotmail.com>
2022-02-25 12:51:43 -0300

    test: ensure AuthorizeHttpGetClientDecorator merge token to HttpGetClient

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
@@ -45,14 +45,29 @@ describe('AuthorizeHttpGetClientDecorator', () => {
     const { sut, getStorageSpy, httpGetClientSpy } = makeSut()
     getStorageSpy.value = mockAccountModel()
     const httpRequest: HttpGetParams = {
+      url: faker.internet.url()
+    }
+    await sut.get(httpRequest)
+    expect(httpGetClientSpy.url).toBe(httpRequest.url)
+    expect(httpGetClientSpy.headers).toEqual({
+      'x-access-token': getStorageSpy.value.accessToken
+    })
+  })
+
+  test('Should merge headers to HttpGetClient', async () => {
+    const { sut, getStorageSpy, httpGetClientSpy } = makeSut()
+    getStorageSpy.value = mockAccountModel()
+    const field = faker.random.words()
+    const httpRequest: HttpGetParams = {
       url: faker.internet.url(),
       headers: {
-        field: faker.random.words()
+        field: field
       }
     }
     await sut.get(httpRequest)
     expect(httpGetClientSpy.url).toBe(httpRequest.url)
     expect(httpGetClientSpy.headers).toEqual({
+      field,
       'x-access-token': getStorageSpy.value.accessToken
     })
   })
