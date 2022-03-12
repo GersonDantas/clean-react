92725a5801fc91d8c169e1fde85718359dd554b6
Gerson Dantas <gerson.accari@hotmail.com>
2022-03-12 16:25:07 -0300

    fix: change name url api

--- src/main/test/cypress/support/survey-list-mocks.ts
+++ src/main/test/cypress/support/survey-list-mocks.ts
@@ -1,5 +1,5 @@
 import * as Http from './http-mocks'
 import faker from 'faker'
 
-export const mockUnexpectedError = (): void => Http.mockServerError(/login/, 'GET')
-export const mockAccessDeniedError = (): void => Http.mockForbiddenError(/login/, 'GET')
+export const mockUnexpectedError = (): void => Http.mockServerError(/surveys/, 'GET')
+export const mockAccessDeniedError = (): void => Http.mockForbiddenError(/surveys/, 'GET')
