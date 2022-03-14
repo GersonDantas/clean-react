c68ba6da07dbdb3ba73746c4df9932b6065799f7
Gerson Dantas <gerson.accari@hotmail.com>
2022-03-14 17:36:22 -0300

    feat: add cdn to external dependeci

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const app = express()
 const root = `${__dirname}/dist`
 app.use(express.static(root))
 app.use(fallback('index.html', { root }))
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Project running...')
+})
