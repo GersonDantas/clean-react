af84a1b12b68e9c25f0802a6d5d70453411a2b77
Gerson Dantas <gerson.accari@hotmail.com>
2022-04-05 10:18:34 -0300

    docs: update react-router-dom with <Outlet />

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -4,6 +4,7 @@ import { MakeLogin, MakeSignUp, MakeSurveyList } from '@/main/factories/pages'
 import { ApiContext } from '@/presentation/context'
 import { PrivateRoute } from '@/presentation/components'
 import React from 'react'
+import { SurveyResult } from '@/presentation/pages'
 
 const Router: React.FC = () => {
   return (
@@ -17,14 +18,10 @@ const Router: React.FC = () => {
         <Routes>
           <Route path="/login" element={<MakeLogin />} />
           <Route path="/signup" element={<MakeSignUp />} />
-          <Route
-            path="/"
-            element={
-              <PrivateRoute>
-                <MakeSurveyList />
-              </PrivateRoute>
-            }
-          />
+          <Route path="/" element={<PrivateRoute />} >
+            <Route path="" element={<MakeSurveyList />} />
+            <Route path="surveys" element={<SurveyResult />} />
+          </Route >
         </Routes>
       </BrowserRouter>
     </ApiContext.Provider>
