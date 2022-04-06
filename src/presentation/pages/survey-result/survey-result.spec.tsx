c55f45b3bd597a327e2e34fbdfd8ac9035cc4f24
Gerson Dantas <gerson.accari@hotmail.com>
2022-04-05 22:15:43 -0300

    refactor: move SUT creation toa factory method

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -6,16 +6,20 @@ import React from 'react'
 import { Router } from 'react-router-dom'
 import { createMemoryHistory } from 'history'
 
+const makeSut = (): void => {
+  const history = createMemoryHistory({ initialEntries: ['/surveys'] })
+  render(
+    <ApiContext.Provider value={{ setCurrentAccount: jest.fn(), getCurrentAccount: () => mockAccountModel() }}>
+      <Router navigator={history} location='/surveys' >
+        <SurveyResult />
+      </Router>
+    </ApiContext.Provider>
+  )
+}
+
 describe('SurveyResult Component', () => {
   test('Should present correct initial state', async () => {
-    const history = createMemoryHistory({ initialEntries: ['/surveys'] })
-    render(
-      <ApiContext.Provider value={{ setCurrentAccount: jest.fn(), getCurrentAccount: () => mockAccountModel() }}>
-        <Router navigator={history} location='/surveys' >
-          <SurveyResult />
-        </Router>
-      </ApiContext.Provider>
-    )
+    makeSut()
     const surveyResult = screen.getByTestId('survey-result')
     expect(surveyResult.childElementCount).toBe(0)
     expect(screen.queryByTestId('error')).not.toBeInTheDocument()
