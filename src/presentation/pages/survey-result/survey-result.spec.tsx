f36482e359e91c7764d60477bacd48929f564e64
Gerson Dantas <gerson.accari@hotmail.com>
2022-04-05 22:13:44 -0300

    test: ensure SurveyResult present correct initial state

---
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -0,0 +1,25 @@
+import { ApiContext } from '@/presentation/context'
+import { SurveyResult } from '@/presentation/pages'
+import { mockAccountModel } from '@/domain/test'
+import { waitFor, render, screen } from '@testing-library/react'
+import React from 'react'
+import { Router } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
+
+describe('SurveyResult Component', () => {
+  test('Should present correct initial state', async () => {
+    const history = createMemoryHistory({ initialEntries: ['/surveys'] })
+    render(
+      <ApiContext.Provider value={{ setCurrentAccount: jest.fn(), getCurrentAccount: () => mockAccountModel() }}>
+        <Router navigator={history} location='/surveys' >
+          <SurveyResult />
+        </Router>
+      </ApiContext.Provider>
+    )
+    const surveyResult = screen.getByTestId('survey-result')
+    expect(surveyResult.childElementCount).toBe(0)
+    expect(screen.queryByTestId('error')).not.toBeInTheDocument()
+    expect(screen.queryByTestId('loading')).not.toBeInTheDocument()
+    await waitFor(() => surveyResult)
+  })
+})
