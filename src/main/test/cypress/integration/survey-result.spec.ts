202e26df9b3fc4fad67cbc6165533b152dbeea3b
Gerson Dantas <gerson.accari@hotmail.com>
2022-04-08 16:16:18 -0300

    test: ensure SurveyResult presnts error if request fails

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -1,7 +1,7 @@
 import * as Helpers from '../utils/helpers'
 import * as Http from '../utils/http-mocks'
 
-const path = /surveys/
+const path = /surveys\/any_id\/result/
 export const mockUnexpectedError = (): void => Http.mockServerError(path, 'GET')
 
 describe('SurveyResult', () => {
