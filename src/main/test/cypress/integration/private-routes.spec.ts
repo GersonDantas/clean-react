b69bc5d269fefc3403d4606d2b44fd02ac385bbb
Gerson Dantas <gerson.accari@hotmail.com>
2022-04-08 12:43:32 -0300

    test: ensure SurveyResult redirect to login if no token is found

--- src/main/test/cypress/integration/private-routes.spec.ts
+++ src/main/test/cypress/integration/private-routes.spec.ts
@@ -1,9 +1,13 @@
-import * as FormHelper from '../utils/form-helpers'
 import * as Helper from '../utils/helpers'
 
 describe('PrivateRoute', () => {
   it('Should logout if survey-list has no token', () => {
     cy.visit('')
+    Helper.testUrl('/login')
+  })
+
+  it('Should logout if survey-list has no token', () => {
+    cy.visit('/surveys/any_id')
     Helper.testUrl('/login')
   })
 })
