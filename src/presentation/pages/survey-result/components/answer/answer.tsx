3cb3c894ddd3bc6b028cc2e0d27074d83c62dc76
Gerson Dantas <gerson.accari@hotmail.com>
2022-04-08 17:04:13 -0300

    fix: correcting typo

--- src/presentation/pages/survey-result/components/answer/answer.tsx
+++ src/presentation/pages/survey-result/components/answer/answer.tsx
@@ -14,7 +14,7 @@ type Props = {
 const Answer: React.FC<Props> = ({ answer }: Props) => {
   const activeClassName = answer.isCurrentAccountAnswer ? Styles.active : ''
   return (
-    <li data-testid='answer-wrap' className={[activeClassName, Styles.answer].join(' ')}>
+    <li data-testid='answer-wrap' className={[activeClassName, Styles.answerWrap].join(' ')}>
       {answer.image && <img data-testid='image' src={answer.image} alt={answer.answer} />}
       <span data-testid='answer' className={Styles.answer}>{answer.answer}</span>
       <span data-testid='percent' className={Styles.percent}>{answer.percent}%</span>
