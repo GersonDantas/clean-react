514fefe18c9d35cbc0cff87a00e980233e7f892d
Gerson Dantas <gerson.accari@hotmail.com>
2022-04-08 15:04:33 -0300

    fix: remove unnecessary params

---
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -0,0 +1,19 @@
+import * as Helpers from '../utils/helpers'
+import * as Http from '../utils/http-mocks'
+
+const path = /surveys/
+export const mockUnexpectedError = (): void => Http.mockServerError(path, 'GET')
+
+describe('SurveyResult', () => {
+  beforeEach(() => {
+    cy.fixture('account').then(account => {
+      Helpers.setLocalStorageItem('account', account)
+    })
+  })
+
+  it('Should present error on UnexpectedError', () => {
+    mockUnexpectedError()
+    cy.visit('/surveys/any_id')
+    cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve')
+  })
+})
--- src/presentation/components/private-route/private-route.tsx
+++ src/presentation/components/private-route/private-route.tsx
@@ -2,7 +2,7 @@ import { ApiContext } from '@/presentation/context'
 import { Navigate, Outlet } from 'react-router-dom'
 import React, { useContext } from 'react'
 
-const PrivateRoute: React.FC = ({ children }): any => {
+const PrivateRoute: React.FC = (): any => {
   const { getCurrentAccount } = useContext(ApiContext)
   return getCurrentAccount()?.accessToken
     ? <Outlet />
