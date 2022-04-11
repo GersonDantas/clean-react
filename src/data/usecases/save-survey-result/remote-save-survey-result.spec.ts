29e88144c40125604d91a9e536948211bab9a5b8
Gerson Dantas <gerson.accari@hotmail.com>
2022-04-11 09:43:09 -0300

    test: ensure RemoteSaveSurveyResult calls with correct values

---
+++ src/data/usecases/save-survey-result/remote-save-survey-result.spec.ts
@@ -0,0 +1,32 @@
+import { HttpStatusCode } from '@/data/protocols/http'
+import { HttpClientSpy, mockRemoteSurveyResultModel } from '@/data/test'
+import { RemoteSaveSurveyResult } from '@/data/usecases'
+import faker from 'faker'
+
+type SutType = {
+  sut: RemoteSaveSurveyResult
+  httpClientSpy: HttpClientSpy
+}
+
+const makeSut = (url = faker.internet.url()): SutType => {
+  const httpClientSpy = new HttpClientSpy()
+  const sut = new RemoteSaveSurveyResult(url, httpClientSpy)
+  return {
+    sut,
+    httpClientSpy
+  }
+}
+
+describe('RemoteSaveSurveyResult', () => {
+  test('Should call HttpClient with correct values ', async () => {
+    const url = faker.internet.url()
+    const { sut, httpClientSpy } = makeSut(url)
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: mockRemoteSurveyResultModel()
+    }
+    await sut.save({ answer: faker.random.word() })
+    expect(httpClientSpy.url).toBe(url)
+    expect(httpClientSpy.method).toBe('put')
+  })
+})
