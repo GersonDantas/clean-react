f2ea3d8b8b47c9703ef81e41d0df8ef4ba8117fc
Gerson Dantas <gerson.accari@hotmail.com>
2022-04-11 13:37:24 -0300

    feat: ensure RemoteSaveSurveyResult returns UnexpectedError

--- src/data/usecases/save-survey-result/remote-save-survey-result.ts
+++ src/data/usecases/save-survey-result/remote-save-survey-result.ts
@@ -1,6 +1,6 @@
 import { RemoteSurveyResultModel } from '@/data/models'
 import { HttpClient, HttpStatusCode } from '@/data/protocols/http'
-import { AccessDeniedError } from '@/domain/errors'
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
 import { SaveSurveyResult } from '@/domain/usecases'
 
 export class RemoteSaveSurveyResult implements SaveSurveyResult {
@@ -17,7 +17,8 @@ export class RemoteSaveSurveyResult implements SaveSurveyResult {
     })
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok: return null
-      default: throw new AccessDeniedError()
+      case HttpStatusCode.forbidden: throw new AccessDeniedError()
+      default: throw new UnexpectedError()
     }
   }
 }
