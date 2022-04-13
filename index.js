aabf005b5b100ef27a6657b46376c5a1cd95b93e
Gerson Dantas <gerson.accari@hotmail.com>
2022-04-13 10:13:47 -0300

    refactor: disable x-power-by header vulnerability

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fallback = require('express-history-api-fallback')
+const path = require('path')
 const express = require('express')
 const app = express()
-const root = `${__dirname}/dist`
+app.disable('x-powered-by')
+const root = path.join(__dirname, '/dist')
 app.use(express.static(root))
 app.use(fallback('index.html', { root }))
 app.listen(process.env.PORT || 3000, () => {
