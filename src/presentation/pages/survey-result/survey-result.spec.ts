17445987802daf0cda8469280648249475f85b60
Gerson Dantas <gerson.accari@hotmail.com>
2022-04-16 14:45:26 -0300

    fix: change test to prevent multiple clicks

--- src/presentation/pages/survey-result/survey-result.spec.ts
+++ src/presentation/pages/survey-result/survey-result.spec.ts
@@ -1,3 +1,4 @@
+import { surveyResultState } from '@/presentation/pages/survey-result/components'
 import { SurveyResult } from '@/presentation/pages'
 import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
 import { AccountModel } from '@/domain/models'
@@ -6,6 +7,7 @@ import { LoadSurveyResultSpy, SaveSurveyResultSpy, mockSurveyResultModel } from 
 import { waitFor, screen, fireEvent } from '@testing-library/react'
 import { createMemoryHistory, MemoryHistory } from 'history'
 import { renderWithHistory } from '@/presentation/test'
+import { LoadSurveyResult } from '@/domain/usecases'
 
 type SutTypes = {
   loadSurveyResultSpy: LoadSurveyResultSpy
@@ -17,11 +19,18 @@ type SutTypes = {
 type SutParams = {
   loadSurveyResultSpy?: LoadSurveyResultSpy
   saveSurveyResultSpy?: SaveSurveyResultSpy
+  initialState?: {
+    isLoading: boolean
+    error: string
+    surveyResult: LoadSurveyResult.Model
+    reload: boolean
+  }
 }
 
 const makeSut = ({
   loadSurveyResultSpy = new LoadSurveyResultSpy(),
-  saveSurveyResultSpy = new SaveSurveyResultSpy()
+  saveSurveyResultSpy = new SaveSurveyResultSpy(),
+  initialState = null
 }: SutParams = {}): SutTypes => {
   const history = createMemoryHistory()
   const { setCurrentAccountMock } = renderWithHistory({
@@ -29,6 +38,7 @@ const makeSut = ({
       loadSurveyResult: loadSurveyResultSpy,
       saveSurveyResult: saveSurveyResultSpy
     }),
+    states: initialState ? [{ atom: surveyResultState, value: initialState }] : [],
     history,
     location: '/surveys'
   })
@@ -199,13 +209,17 @@ describe('SurveyResult Component', () => {
   })
 
   test('Should present multiple answer click', async () => {
-    const { saveSurveyResultSpy } = makeSut()
+    const initialState = {
+      isLoading: true,
+      error: '',
+      surveyResult: null,
+      reload: false
+    }
+    const { saveSurveyResultSpy } = makeSut({ initialState })
     await waitFor(() => screen.getByTestId('survey-result'))
     const answerWrap = screen.queryAllByTestId('answer-wrap')
     fireEvent.click(answerWrap[1])
     await waitFor(() => screen.getByTestId('survey-result'))
-    fireEvent.click(answerWrap[1])
-    await waitFor(() => screen.getByTestId('survey-result'))
-    expect(saveSurveyResultSpy.callsCount).toBe(1)
+    expect(saveSurveyResultSpy.callsCount).toBe(0)
   })
 })
--- src/presentation/test/render-helper.tsx
+++ src/presentation/test/render-helper.tsx
@@ -5,7 +5,7 @@ import { mockAccountModel } from '@/domain/test'
 import { render } from '@testing-library/react'
 import { Router } from 'react-router-dom'
 import { MemoryHistory } from 'history'
-import { RecoilRoot } from 'recoil'
+import { MutableSnapshot, RecoilRoot, RecoilState } from 'recoil'
 import React from 'react'
 
 type Params = {
@@ -13,17 +13,21 @@ type Params = {
   history: MemoryHistory
   account?: AccountModel
   location: string
+  states?: Array<{ atom: RecoilState<any>, value: any }>
 }
 
 type Result = {
   setCurrentAccountMock: (account: AccountModel) => void
 }
 
-export const renderWithHistory = ({ Page, account = mockAccountModel(), ...props }: Params): Result => {
+export const renderWithHistory = ({ Page, account = mockAccountModel(), states = [], ...props }: Params): Result => {
   const setCurrentAccountMock = jest.fn()
   const mockedState = { setCurrentAccount: setCurrentAccountMock, getCurrentAccount: () => account }
+  const initializeState = ({ set }: MutableSnapshot): void => {
+    [...states, { atom: currentAccountState, value: mockedState }].forEach(state => set(state.atom, state.value))
+  }
   render(
-    <RecoilRoot initializeState={({ set }) => set(currentAccountState, mockedState)}>
+    <RecoilRoot initializeState={initializeState}>
       <Router location={props.location} navigator={props.history}>
         <Page />
       </Router>
