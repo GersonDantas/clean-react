f7139ac9eb7cf21529bf15fd7c589748448e6aca
Gerson Dantas <gerson.accari@hotmail.com>
2022-05-16 18:59:45 -0300

    feat: ensure ValidationBulder returns EmailValidation

--- tests/main/builders/validation-builder.spec.ts
+++ tests/main/builders/validation-builder.spec.ts
@@ -1,13 +1,21 @@
 import { ValidationBuilder as sut } from '@/main/builders'
-import { RequiredFieldValidation } from '@/validation/validators'
+import { RequiredFieldValidation, EmailValidation } from '@/validation/validators'
 import faker from 'faker'
 
 describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
+    const field = faker.database.column()
+
+    const validators = sut.field(field).required().build()
+
+    expect(validators).toEqual([new RequiredFieldValidation(field)])
+  })
+
+  test('Should return EmailValidation', () => {
     const email = faker.database.column()
 
-    const validators = sut.field(email).required().build()
+    const validators = sut.field(email).email().build()
 
-    expect(validators).toEqual([new RequiredFieldValidation(email)])
+    expect(validators).toEqual([new EmailValidation(email)])
   })
 })
