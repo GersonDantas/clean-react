9d9d990f542587795ab64c9a03d591db670cee3b
Gerson Dantas <gerson.accari@hotmail.com>
2022-05-16 19:03:06 -0300

    feat: ensure ValidationBulder returns MinLengthValidation

--- tests/main/builders/validation-builder.spec.ts
+++ tests/main/builders/validation-builder.spec.ts
@@ -1,5 +1,5 @@
 import { ValidationBuilder as sut } from '@/main/builders'
-import { RequiredFieldValidation, EmailValidation } from '@/validation/validators'
+import { RequiredFieldValidation, EmailValidation, MinLengthValidation } from '@/validation/validators'
 import faker from 'faker'
 
 describe('ValidationBuilder', () => {
@@ -17,5 +17,14 @@ describe('ValidationBuilder', () => {
     const validators = sut.field(email).email().build()
 
     expect(validators).toEqual([new EmailValidation(email)])
+  })
+
+  test('Should return MinLengthValidation', () => {
+    const field = faker.database.column()
+    const length = faker.datatype.number(14)
+
+    const validators = sut.field(field).min(length).build()
+
+    expect(validators).toEqual([new MinLengthValidation(field, length)])
   })
 })
